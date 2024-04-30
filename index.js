e3537336036b9b0295c10c1eac13c360590665aa
LightLord <stevensah88@gmail.com>
2024-04-30 08:18:57 -0700

    hope it fixes

--- index.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('background-select').addEventListener('change', () => {
-    const selectedBackground = document.getElementById('background-select').value;
-    document.body.style.backgroundImage = `url(./${selectedBackground}.jpg)`;
-    document.body.style.backgroundSize = 'cover';
-});
---
+++ src/index.js
@@ -0,0 +1,5 @@
+document.getElementById('background-select').addEventListener('change', () => {
+    const selectedBackground = document.getElementById('background-select').value;
+    document.body.style.backgroundImage = `url(./assets/${selectedBackground}.jpg)`;
+    document.body.style.backgroundSize = 'cover';
+});
